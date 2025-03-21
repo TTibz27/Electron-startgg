e8b8667184c7368d6b013b6e18daf337f52737b7
Tibz <ttibble90@gmail.com>
2025-03-20 21:23:13 -0500

    fixed null error

--- backend/ObsCaptureViews/Top8/top8view.js
+++ backend/ObsCaptureViews/Top8/top8view.js
@@ -43,22 +43,29 @@ function handleMessage(msg){
     if (data.set_id === "E" && data.player1_name !== "" && data.player2_name !== "" ){
         document.getElementById("GrandFinalsReset").classList.remove('HideGFR');
     }
+    console.log
+    const player1name = data.player1_name;
+    const player2name = data.player2_name;
+    console.log(data.player1_score);
+      console.log( typeof data.player1_score === 'number');
+    const p1Score =  typeof data.player1_score !== 'number'? "" : data.player1_score;
+    const ps2Score=  typeof data.player2_score !== 'number'? "" : data.player2_score;
 
 
+    document.getElementById(p1NameSpanId).innerHTML = player1name + "";
+    document.getElementById(p1ScoreSpanId).innerHTML = p1Score + "";
+    document.getElementById(p2NameSpanId).innerHTML =  player2name + "";
+    document.getElementById(p2ScoreSpanId).innerHTML =ps2Score + "";
 
-    document.getElementById(p1NameSpanId).innerHTML = data.player1_name + "";
-    document.getElementById(p1ScoreSpanId).innerHTML = data.player1_score + "";
-    document.getElementById(p2NameSpanId).innerHTML = data.player2_name + "";
-    document.getElementById(p2ScoreSpanId).innerHTML = data.player2_score + "";
-
-
-    if (data.player1_score > data.player2_score){
-        const element = document.getElementById(p2SetSpanId); // Select the element
-        element.classList.add('lossIndication'); // Add the class 'new-class'
-    }
-    else if (data.player2_score > data.player1_score){
-        const element = document.getElementById(p1SetSpanId); // Select the element
-        element.classList.add('lossIndication'); // Add the class 'new-class'
+    if(data.player1_score === 3 || data.player2_score === 3){
+        if (data.player1_score > data.player2_score){
+            const element = document.getElementById(p2SetSpanId); // Select the element
+            element.classList.add('lossIndication'); // Add the class 'new-class'
+        }
+        else if (data.player2_score > data.player1_score){
+            const element = document.getElementById(p1SetSpanId); // Select the element
+            element.classList.add('lossIndication'); // Add the class 'new-class'
+        }
     }
   }
 }
